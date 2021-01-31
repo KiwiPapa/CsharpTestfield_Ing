30c94b1bf215afcc70d3333e4f2ec4e3692a30c4
KiwiPapa <bmwmsn123@icloud.com>
2021-01-31 22:29:01 +0800

    WinForm应用程序之GDI绘图

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen pen = new Pen(Brushes.Red);
+            //指定起始点
+            Point p1 = new Point(30, 50);
+            Point p2 = new Point(200, 300);
+            //画
+            g.DrawLine(pen, p1, p2);
+        }
+        int i = 0;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            i++;
+            label1.Text = i.ToString();
+            Graphics g = this.CreateGraphics();
+            Pen pen = new Pen(Brushes.Blue);
+            //指定起始点
+            Point p1 = new Point(30, 50);
+            Point p2 = new Point(200, 300);
+            //画
+            g.DrawLine(pen, p1, p2);
+        }
+    }
+}

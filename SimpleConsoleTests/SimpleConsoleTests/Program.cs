39386578f3fb1b2887b24a67f1e4d7b7c6c18135
KiwiPapa <bmwmsn123@icloud.com>
2021-02-18 22:27:06 +0800

    add

---
+++ SimpleConsoleTests/SimpleConsoleTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    Console.WriteLine(i.ToString() + " " + j.ToString());
+                }
+            }
+        }
+    }
+}
